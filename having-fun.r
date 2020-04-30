92dcb96acabbc7a861cf17ca7543e51cfa8bc0ca
clovesgtx <clovesgtx@gmail.com>
2020-04-30 08:13:05 -0300

    ...

--- having-fun.r
+++ having-fun.r
@@ -36,4 +36,28 @@ isin(x, c(3, 5, 4))
 S <- rolldie(4)
 subset(S, isin(S, c(2, 2, 6), ordered = TRUE))
 
+# Set Union, Intersection, and Difference
+S <- cards()
+A <- subset(S, suit == "Heart")
+B <- subset(S, rank %in% 7:9)
 
+union(A, B)
+intersect(A, B)
+setdiff(A, B)
+setdiff(B, A)
+setdiff(S, A) # complement of A
+
+outcomes <- rolldie(1)
+p <- rep(1/6, times = 6)
+probspace(outcomes, probs = p)
+# or
+probspace(1:6, probs = p)
+# or
+probspace(1:6)
+# or
+rolldie(1, makespace = T)
+# or
+rolldie(1, T)
+
+# An unbalanced coin
+probspace(tosscoin(1), probs = c(0.7, 0.3))
